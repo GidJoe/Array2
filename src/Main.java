63046671d004f7e3ff512b4c86bbb6f4b365df0e
GidJoe <winterm@bfwob.de>
2023-01-08 07:06:05 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,21 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-            Random ran = new Random();
-            int x = 10 + ran.nextInt(49 - 1 + 1);
-            int[] lottozahlen = new int[6];
-            System.out.println(Arrays.toString(lottozahlen));
+        Random ran = new Random();
+        int x = 10 + ran.nextInt(49 - 1 + 1);
+        int[] lottozahlen = new int[6];
+        System.out.println(Arrays.toString(lottozahlen));
 
-        for (int i=0; i<6; i++) {
+        for (int i = 0; i < 6; i++) {
 
             x = 1 + ran.nextInt(50 - 1 + 1);
             lottozahlen[i] = x;
-            System.out.println((i+1) + ". Ziehung : "+x);
+            System.out.println((i + 1) + ". Ziehung : " + x);
 
         }
-            int e = 0;
-            int z = 0;
-            boolean schleife = true;
 
-            System.out.println("Die Lottozahlen diese Woche lauten: " + Arrays.toString(lottozahlen));
 
-        }
+    }
 }
 
 
